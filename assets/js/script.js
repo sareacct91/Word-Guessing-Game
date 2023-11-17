6c63b9db88ef91dab5de4e41674daee1bbd4bd68
Meow Roar <sareacct91@gmail.com>
2023-11-16 23:18:10 -0800

    Add comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ let score = JSON.parse(localStorage.getItem("score")) || {win: 0, lose: 0};
 let intervalId;
 let isStart = false;
 
-// Get words list from words.json file
+// fetch words list from words.json file
 async function fetchWordsArr() {
   const response = await fetch("./assets/data/words.JSON");
   wordsArr = await response.json();
@@ -25,50 +25,56 @@ function displayScore() {
 }
 displayScore();
 
-// When end condition is met
+// End the game, isWin = true when the function is called from the eventListener
 function endGame(isWin = false) {
+  // Set game state to false and stop the timer
   isStart = false;
   clearInterval(intervalId);
-  timeDisplay.textContent = '';
 
+  // if you win, show winning text and win++, else show losing text and lose++
   isWin ? (resultDisplay.textContent = "You Win", score.win++) : (resultDisplay.textContent = "You Lose", score.lose++);
   localStorage.setItem("score", JSON.stringify(score));
 
+  // Display the new score
   displayScore();
+
+  // Show the play button again
   startBtn.textContent = "Play again";
   startBtn.classList.remove("hide");
 }
 
 // Start the gameas
 function startGame() {
+  // Set game state to true
   isStart = true;
 
   // Return random word from the words array
   const word = wordsArr[Math.floor(Math.random() * wordsArr.length)];
   console.log(word);
 
+  // Add div elements equal to the length of the chosen word to divArr
   for (let i = 0; i < word.length; i++) {
     const divEl = document.createElement("div");
 
-    divEl.dataset.state = "hide";
     divEl.dataset.char = word[i];
     divEl.textContent = "_";
 
     divArr.push(divEl);
   }
-  // display word on screen
+  // display div elements on the page
   divArr.forEach(element => wordDisplay.appendChild(element));
 
   // Start the timer, end game if time is 0
   let totalTime = 10;
 
   intervalId = setInterval(function time() {
-    return totalTime === 0 ? endGame() : (timeDisplay.textContent = totalTime--, time);
+    return totalTime === 0 ? (timeDisplay.textContent = totalTime, endGame()) : (timeDisplay.textContent = totalTime--, time);
   }(),1000);
 }
 
 // When clicked, start the game
 startBtn.addEventListener('click', () => {
+  // Hide the start button 
   startBtn.classList.add("hide");
 
   // Reset the game
@@ -82,20 +88,24 @@ startBtn.addEventListener('click', () => {
 
 // Check for keypress, 
 document.addEventListener('keydown', (event) => {
-  if (isStart) {
-    let correctArr = divArr.filter(element => event.key.toLowerCase() === element.dataset.char.toLowerCase());
+  if (!isStart) {return}
 
-    if (correctArr.length !== 0) {
-      correctArr.forEach(element => element.textContent = element.dataset.char);
-    }
+  // correctArr =  filter out elements from divArr that have the correct character
+  let correctArr = divArr.filter(element => event.key.toLowerCase() === element.dataset.char.toLowerCase());
 
-    if (!divArr.some(element => element.textContent === "_")) {
-      console.log('finish');
-      endGame(true); 
-    }
+  // If correctArr have elements in it, set the textContent of those elements to the correct key
+  if (correctArr.length !== 0) {
+    correctArr.forEach(element => element.textContent = element.dataset.char);
+  }
+
+  // If there's no more element that's "hidden", end game
+  if (!divArr.some(element => element.textContent === "_")) {
+    console.log('finish');
+    endGame(true); 
   }
 });
 
+// Reset the score in localStorage and on the page
 document.querySelector("#resetBtn").addEventListener("click", () => {
   localStorage.clear();
   winDisplay.textContent = "0";
